461d09746ed6fc52c046626435f6ff0ae17c67a7
SamRithvik <88350852+SamRithvik@users.noreply.github.com>
2021-11-29 18:28:53 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -66,7 +66,7 @@ image(paperImg, ball.position.x,ball.position.y,radius,radius);
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-		Matter.Body.applyForce(ball,ball.position,{x:85,y:-85});
+		Matter.Body.applyForce(ball,ball.position,{x:45,y:-65});
     
   	}
 }
